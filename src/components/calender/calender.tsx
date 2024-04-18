8fceca0b4228e72fb9788ef64236276009ca1511
cmkim <cmkim1003@revu.net>
2024-04-18 17:54:03 +0900

    캘린더 추가

--- src/components/calender/calender.tsx
+++ src/components/calender/calender.tsx
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from 'styled-components';
 import moment from "moment";
-import { useParams } from 'react-router-dom';
 
 const CalenderWrap = styled.div`
    text-align: center;
@@ -12,33 +11,95 @@ const CalenderInner = styled.div`
    color:blue;
 `;
 
-const Calender = () => {
-    const { type } = useParams();
-    const currentYear: any = moment().year();
-    const currentMonth: any = moment().month() + 1;
-    const currentDate: any = moment();
-    let dayNames: any;
-    let weeks: any;
+const CalendarComponent = () => {
+    const [currYear, setCurrYear] = useState<number>(moment().year());
+    const [currMonth, setCurrMonth] = useState<number>(moment().month() + 1);
+    const [currentDate, setCurrentDate] = useState<moment.Moment>(moment());
+    const [dayNames] = useState<string[]>(['일', '월', '화', '수', '목', '금', '토']);
+    const [weeks, setWeeks] = useState<any[][]>([]);
 
-    console.log(currentYear, currentMonth)
+    useEffect(()=> {
+        makeCalendar();
+    },[currMonth, currYear]);
 
+    const beforeMonth = () => {
+        setCurrentDate(prevDate => prevDate.subtract(1, 'months'));
+        setCurrMonth(currentDate.month() + 1);
+    };
 
-    return (
-        <CalenderWrap>
-            {type ==='me'?
-                <CalenderInner>
-                    <h4>본인</h4>
-                </CalenderInner>
-                :
-                <CalenderInner>
-                    <h4>본인아님</h4>
-                </CalenderInner>
+    const nextMonth = () => {
+        setCurrentDate(prevDate => prevDate.add(1, 'months'));
+        setCurrMonth(currentDate.month() + 1);
+    };
+
+    const makeCalendar = () => {
+        const daysInMonth = moment([currYear, currMonth - 1]).daysInMonth();
+        const firstDay = moment([currYear, currMonth - 1, 1]).day();
+
+        let newWeeks: number[][] = [];
+        let week: any[] = [];
+
+        for (let i = 0; i < firstDay; i++) {
+            week.push(''); // Placeholder for empty cells
+        }
+
+        for (let day = 1; day <= daysInMonth; day++) {
+            week.push(day);
+
+            if (week.length === 7) {
+                newWeeks.push(week);
+                week = [];
             }
+        }
+
+        if (week.length > 0) {
+            while (week.length < 7) {
+                week.push('');
+            }
+            newWeeks.push(week);
+        }
+
+        setWeeks(newWeeks);
+    };
+
+
+    const selectedDay = (day: number) => {
+        const today = moment().format('YYYY-MM-DD');
+        const setDay = moment([currYear, currMonth - 1, day]).format('YYYY-MM-DD');
+
+        if (today === setDay) {
+            const selectedTd = document.getElementById('id-' + day);
+            if (selectedTd) {
+                selectedTd.classList.add('clickable');
+            }
+        }
+    };
+
+     return (
+        <div>
+            <button onClick={beforeMonth}>이전 달</button>
+            <button onClick={nextMonth}>다음 달</button>
             <table>
+                <thead>
+                    <tr>
+                        {dayNames.map((name, index) => (
+                            <th key={index}>{name}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {weeks.map((week, index) => (
+                        <tr key={index}>
+                            {week.map((day, index) => (
+                                <td key={index} onClick={() => selectedDay(day)}>
+                                    {day}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
             </table>
-        </CalenderWrap>
-    )
-}
+        </div>
 
-
-export default Calender;
+)};
+export default CalendarComponent;
